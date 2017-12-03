231f19f8772447e03c8a022e508e7e2764a3971a
fjn <fjn@163.com>
2017-12-03 13:19:47 +0800

    请求路径处理

--- src/cn/mldn/fjn/servlet/DispatcherServlet.java
+++ src/cn/mldn/fjn/servlet/DispatcherServlet.java
@@ -1,21 +1,45 @@
 package cn.mldn.fjn.servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.mldn.fjn.web.RequestUriUtil;
+
 @SuppressWarnings("serial")
-public class DispatcherServlet extends HttpServlet{
+@WebServlet("*.action")
+@WebFilter("/pages/back/admin/emp/*")
+public class DispatcherServlet extends HttpServlet implements Filter{
+
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		String temp[]=RequestUriUtil.splitUri(request);
+		System.out.println(Arrays.toString(temp));
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		this.doPost(req, resp);
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+			HttpServletRequest request=(HttpServletRequest)req;
+			request.setCharacterEncoding("UTF-8");
+			chain.doFilter(req, resp);
+			resp.setCharacterEncoding("UTF-8");
 	}
 }
---
+++ src/cn/mldn/fjn/web/RequestUriUtil.java
@@ -0,0 +1,28 @@
+package cn.mldn.fjn.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 定义一个专门的程序类，用来拆分路径，返回Action的路径以及方法名称！
+ * @author lenovo
+ *
+ */
+public class RequestUriUtil {
+	private RequestUriUtil() {}
+	
+	/**
+	 * 进行给定路径的拆分处理操作，路径的组成，XxxAction方法！方法名称.action
+	 * @param request 请求对象
+	 * @return 返回的信息里面包含有两个参数
+	 * 第一个参数:为Action的访问路径
+	 * 第二个参数:为Action类中的调用方法
+	 */
+	public static String[] splitUri(HttpServletRequest request) {
+		String[] temp=new String[2];
+		String uri=request.getRequestURI().replaceFirst(request.getContextPath(), "");
+		String result[]=uri.split("!");
+		temp[0]=result[0];
+		temp[1]=result[1].substring(0,result[1].lastIndexOf("."));
+		return temp;
+	}
+}
